74e7630c620f99d6952b6e694be7c85b6c2c8188
anitapanczel <anita@panczel.hu>
2022-02-16 14:48:33 +0100

    artwork style refine

--- frontend/src/components/artwork.jsx
+++ frontend/src/components/artwork.jsx
@@ -6,16 +6,17 @@ const Artwork = (props) => {
   
   return (
     <>
-      <h1>Title: {title}</h1>
+
       <div key={key} className="artwork">
         <div className="art_image">
           <img src={img} alt="" />
         </div>
         <div className="art_desc">
-          <h3>Creator name: {creator}</h3>
-          <h3>Creation year: {date}</h3>
+          <h1>{title}</h1>
+          <h3>{creator}</h3>
           <p>{desc}</p>
           <p>{funfact}</p>
+          <h5>Creation year: {date}</h5>
           <button onClick={() => window.location.reload()}>Back...</button>
         </div>
       </div>
