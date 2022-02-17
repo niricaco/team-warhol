8631d95b54c745f794a72acda4526229b3a9facf
Skoda130rs <kovacspet@axelero.hu>
2022-02-17 20:56:46 +0100

    login1

--- backend/index.js
+++ backend/index.js
@@ -8,27 +8,27 @@ const port = 3000;
 
 const users = require('./data/users.json')
 
-app.get("/signup", (req, res) => {
+/*app.get("/api/signup", (req, res) => {
   res.send("Hello World!");
-});
+});*/
 
-app.post('/signup', (req, res) => {
-  if (!req.body.name || !req.body.password) {
+app.post('/api/signup', (req, res) => {
+  if (!req.body.email || !req.body.password) {
     return res.status(400).json("Missing credentials")
   }
-  const userExists = users.some((user) => user.name === req.body.name)
+  const userExists = users.some(user => user.email === req.body.email)
   if (userExists) {
-    return res.sendStatus(409)
+    return res.status(409).json("User already exist with an other password")
   }
   const newUser = {
-    name: req.body.name,
+  //  name: req.body.name,
     password: req.body.password,
     email: req.body.email
   }
   users.push(newUser)
 
   fs.writeFileSync('./data/users.json', JSON.stringify(users, null, 4))
-  res.sendStatus(200)
+  res.status(200).json("User registriert")
 })
 
 app.listen(port, () => {
