b7f1e0570d6f26b4b1cc1b5d0361456df619d93b
Kubi <KubiHUN@users.noreply.github.com>
2022-02-18 21:38:45 +0100

    Just the prittier :)

--- frontend/src/components/artworks.jsx
+++ frontend/src/components/artworks.jsx
@@ -6,69 +6,67 @@ import "./style/pagination.css";
 import Artwork from "./artwork";
 import ReactPaginate from "react-paginate";
 
-
 const Artworks = () => {
-    const [artworks, setArtworks] = useState([]);  
-    const [pageNumber, setPageNumber] = useState(0)
+  const [artworks, setArtworks] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
 
-    const artworksPerPage = 20;
-    const pagesVisited = pageNumber * artworksPerPage
+  const artworksPerPage = 20;
+  const pagesVisited = pageNumber * artworksPerPage;
 
-    const displayArts = artworks
-        .slice(pagesVisited, pagesVisited + artworksPerPage)
-        .map((item) => {
-            return (
-          
-            <Artwork
-                    title={item.title}
-                    index={item.accession_number}
-                    image={item.images.web.url}
-                    creator={item.creators.length > 0 ? item.creators[0].description : "Creator unknown"}
-                    date={item.creation_date}
-                    desc={item.wall_description}
-                    funfact={item.fun_fact} 
-            />
-          
-        )});
+  const displayArts = artworks
+    .slice(pagesVisited, pagesVisited + artworksPerPage)
+    .map((item) => {
+      return (
+        <Artwork
+          title={item.title}
+          index={item.accession_number}
+          image={item.images.web.url}
+          creator={
+            item.creators.length > 0
+              ? item.creators[0].description
+              : "Creator unknown"
+          }
+          date={item.creation_date}
+          desc={item.wall_description}
+          funfact={item.fun_fact}
+        />
+      );
+    });
 
-    const pageCount = Math.ceil(artworks.length / artworksPerPage);
-    const changePage = ({selected}) => {
-            setPageNumber(selected);
-            console.log(selected + 1)
-    };
-       
+  const pageCount = Math.ceil(artworks.length / artworksPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+    console.log(selected + 1);
+  };
 
-    const load = async () => {
-        const response = await axios.get(
-            `https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&limit=100&page=${pageNumber}`
-            );
-        setArtworks(response.data.data);
-    };
-    //console.log(artworks);
+  const load = async () => {
+    const response = await axios.get(
+      `https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&limit=100&page=${pageNumber}`
+    );
+    setArtworks(response.data.data);
+  };
+  //console.log(artworks);
 
-    useEffect(() => {
-        load();
-    }, []);
-
+  useEffect(() => {
+    load();
+  }, []);
 
   return (
     <>
-      <div className="grid">
-        {displayArts}
-      </div>
+      <div className="grid">{displayArts}</div>
       <div className="paginate-container">
         <ReactPaginate
-            previousLabel={"Previous Arts"}
-            nextLabel={"Next Arts"}
-            pageCount={pageCount}
-            onPageChange={changePage}
-            containerClassName={"paginationBttns"}
-            previousLinkClassName={"previousBttn"}
-            nextLinkClassName={"nextBttn"}
-            disabledClassName={"paginationDisabled"}
-            activeClassName={"paginationActive"}
+          previousLabel={"Previous Arts"}
+          nextLabel={"Next Arts"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
         />
-    </div>
+      </div>
     </>
   );
 };
@@ -91,18 +89,17 @@ onClick={() => loadartwork(item.accession_number, item.title, item.creators[0].d
                   />}
 */
 
+//console.log(artworks);
+//const titleList = artworks.map((item) => item.title);
+//console.log(titleList);
 
-  //console.log(artworks);
-  //const titleList = artworks.map((item) => item.title);
-  //console.log(titleList);
+//képek az images -> web -> url utvonalon
 
-  //képek az images -> web -> url utvonalon
+//const accessNumList = artworks.map((item) => item.accession_number);
+//console.log(accessNumList);
+//kép elérése: https://openaccess-cdn.clevelandart.org/1915.534/1915.534_web.jpg <--- '1915.534', ezt mind a két helyen {} az accessNumList
 
-  //const accessNumList = artworks.map((item) => item.accession_number);
-  //console.log(accessNumList);
-  //kép elérése: https://openaccess-cdn.clevelandart.org/1915.534/1915.534_web.jpg <--- '1915.534', ezt mind a két helyen {} az accessNumList
-
-  /* <ol></ol>
+/* <ol></ol>
         {artworks.map((item) => (
           <li key={item.accession_number}>{item.title}</li>
         ))}
