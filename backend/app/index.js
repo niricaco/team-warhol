119f1069729bff9851cd94670f756d1ef0d0a89d
Kubi <KubiHUN@users.noreply.github.com>
2022-02-24 23:58:29 +0100

    I`m failed

--- backend/app/index.js
+++ backend/app/index.js
@@ -4,38 +4,11 @@ const port = 4000;
 const cors = require("cors");
 const fs = require("fs");
 
-const winston = require("winston");
-const expressWinston = require("express-winston");
-
 app.use(cors());
 app.use(express.json());
 
-/*const logLevel = "info";
-app.use(
-  expressWinston.logger({
-    transports: [new winston.transports.Console({ level: logLevel })],
-  })
-);*/
-
 //const users = require("./users.json");
 let mySessionStorage = {};
-
-app.post("/api/login", (req, res) => {
-  const users = require("./users.json");
-  const authHeader = req.header("authorization");
-  if (!authHeader) return res.sendStatus(401);
-  const userEmail = authHeader.split(":::")[0];
-  const password = authHeader.split(":::")[1];
-  const user = users.find(
-    (user) => user.email === userEmail && user.password === password
-  );
-  if (!user) {
-    return res.sendStatus(401);
-  }
-  const sessionId = Math.random().toString();
-  mySessionStorage[sessionId] = user;
-  return res.json(sessionId);
-});
 
 app.post("/api/signup", (req, res) => {
   const users = require("./users.json");
@@ -56,9 +29,28 @@ app.post("/api/signup", (req, res) => {
   res.status(200).json("User registered");
 });
 
+app.post("/api/login", (req, res) => {
+  const users = require("./users.json");
+  const authHeader = req.header("authorization");
+  if (!authHeader) return res.sendStatus(401);
+  const userEmail = authHeader.split(":::")[0];
+  const password = authHeader.split(":::")[1];
+  const user = users.find(
+    (user) => user.email === userEmail && user.password === password
+  );
+  if (!user) {
+    return res.sendStatus(401);
+  }
+  const sessionId = Math.random().toString();
+  mySessionStorage[sessionId] = user;
+  return res.json(sessionId);
+});
+
 app.delete("/api/logout", (req, res) => {
   const sessionId = req.header("authorization");
+  console.log(req.header);
   if (!sessionId) return res.sendStatus(401);
+  console.log(mySessionStorage);
   delete mySessionStorage[sessionId];
   res.sendStatus(200);
 });
@@ -103,3 +95,98 @@ app.post("/api/modifyCollection", (req, res) => {
 app.listen(port, () => {
   console.log(`Registration listening on port ${port}`);
 });
+
+//-------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------
+
+/* app.post("/api/signup", (req, res) => {
+    if (!req.body.email || !req.body.password)
+    return res.status(400).json("Missing credentials");
+    fs.readFile("users.json", "", (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+      const users = JSON.parse(data);
+      const userExists = users.some((user) => user.email === req.body.email);
+      if (userExists) return res.status(409).json("User already exist");
+      const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        photos: [],
+      };
+      users.push(newUser);
+      fs.writeFile("users.json", JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+          console.error(err);
+          return res.sendStatus(400);
+        }
+        console.log("//file written successfully");
+        //file written successfully
+      });
+      res.status(200).json("User registered");
+    });
+  }); */
+
+/* app.post("/api/login", (req, res) => {
+    const authHeader = req.header("authorization");
+    if (!authHeader) return res.sendStatus(401);
+    const userEmail = authHeader.split(":::")[0];
+    const password = authHeader.split(":::")[1];
+    fs.readFile("users.json", "", (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+      const users = JSON.parse(data);
+      const user = users.find(
+        (user) => user.email === userEmail && user.password === password
+        );
+        if (!user) return res.sendStatus(401);
+      const sessionId = Math.random().toString();
+      mySessionStorage[sessionId] = user;
+      console.log(mySessionStorage);
+      setTimeout(() => {
+        delete mySessionStorage[sessionId];
+      }, 60 * 60 * 1000);
+      return res.json(sessionId);
+    });
+  }); */
+
+/* app.post("/api/write", (req, res) => {
+      if (!req.body.save) return res.send("Wrong query data");
+      fs.readFile("data.json", "", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        niceData = JSON.parse(data);
+        const toSave = req.body.save;
+        const dataToSave = {
+          id: niceData.length + 1,
+          data: toSave,
+          photos: [],
+        };
+        niceData.push(dataToSave);
+        fs.writeFile("data.json", JSON.stringify(niceData), (err) => {
+          if (err) {
+            console.error(err);
+            return res.sendStatus(400);
+          }
+          //file written successfully
+        });
+      });
+      return res.sendStatus(204);
+    });
+    
+    app.get("/api/get", (req, res) => {
+      fs.readFile("data.json", "", (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.sendStatus(400);
+        }
+        console.log(data);
+        return res.send(data);
+      });
+    }); */
